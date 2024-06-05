80425d1849c545e58905c065a502f7e0c337ebb3
dhlinhdn00 <131579001+dhlinhdn00@users.noreply.github.com>
2024-06-06 02:51:48 +0700

    update ignoring file/foler

--- gen_dir_map.sh
+++ gen_dir_map.sh
@@ -4,14 +4,17 @@ DEPTH=${DIR_MAP_DEPTH:-3}
 
 OUTPUT_FILE=${DIR_MAP_FILE:-dir_map.txt}
 
-tree -n -L $DEPTH -o $OUTPUT_FILE
+EXCLUDE_PATTERN=${DIR_MAP_EXCLUDE:-"folder_to_ignore|file_to_ignore|*.extension_to_ignore"}
+
+tree -n -L $DEPTH -I "$EXCLUDE_PATTERN" -o $OUTPUT_FILE
 
 echo "Directory map has been saved to $(pwd)/$OUTPUT_FILE"
 
 # chmod +x gen_dir_map.sh
 
-# Modify Command:
 # export DIR_MAP_DEPTH=3
 # export DIR_MAP_FILE=dir_map.txt
+# export DIR_MAP_EXCLUDE="folder_to_ignore|file_to_ignore|*.extension_to_ignore"
 
 # ./gen_dir_map.sh
+
